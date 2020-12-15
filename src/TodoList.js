878b78c56dd20fa8b278ddd58614feefc129f6d4
yaocheng <1075168952@qq.com>
2020-12-15 12:01:48 +0800

    瞎搞了eject

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
 
-import store from '../store'
+import store from './store/index'
 
 const data = [
     '早8点开晨会，分配今天的开发工作',
