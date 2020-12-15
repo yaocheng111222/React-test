7cc21042593939a99392b8f3520cb10149f387d1
yaocheng <1075168952@qq.com>
2020-12-15 15:41:30 +0800

    store

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,38 +1,49 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
+import { changeInputAction, addItemAction, deleteItemAction } from './store/actionCreator'
 
 import store from './store/index'
-
-const data = [
-    '早8点开晨会，分配今天的开发工作',
-    '早9点和项目经理作开发需求讨论会',
-    '晚5:30对今日代码进行review'
-]
 class TodoList extends Component {
     constructor(props) {
         super(props)
         this.state = store.getState()
-        console.log(this.state)
+        this.storeChange = this.storeChange.bind(this)  //转变this指向
+        store.subscribe(this.storeChange)
     }
     render() { 
         return (
             <div>
                 <div>
-                    <Input placeholder='jspang' style={{ width: '200px', margin: '20px 0 20px' }} />
+                    <Input placeholder={this.state.inputValue} onChange={this.changeInputValue.bind(this)} style={{ width: '200px', margin: '20px 0 20px' }} />
                 </div>
                 <div>
-                    <Button type="primary">增加</Button>
+                    <Button type="primary" onClick={this.clickBtn.bind(this)}>增加</Button>
                 </div>
                 <div style={{ margin: '10px', width: '300px' }}>
                     <List
                     bordered
-                    dataSource={data}
-                    renderItem={item=>(<List.Item>{item}</List.Item>)}
+                    dataSource={this.state.list}
+                    renderItem={(item,index)=>(<List.Item onClick={this.deleteItem.bind(this,index)}>{item}</List.Item>)}
                     />
                 </div>
             </div>
         );
+    }
+    changeInputValue(e) {
+        const action = changeInputAction(e.target.value)
+        store.dispatch(action)
+    }
+    storeChange() {
+        this.setState(store.getState())
+    }
+    clickBtn() {
+        const action = addItemAction()
+        store.dispatch(action)
+    }
+    deleteItem(index) {
+        const action = deleteItemAction(index)
+        store.dispatch(action)
     }
 }
  
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,4 @@
+import {CHANGE_INPUT,ADD_ITEM,DELETE_ITEM} from './actionTypes'
 const defaultState = {
     inputValue : 'Write Something',
     list:[
@@ -6,5 +7,21 @@ const defaultState = {
     ]
 } // 默认数据
 export default (state = defaultState,action) => {
+    if (action.type == CHANGE_INPUT) {
+        let newState = JSON.parse(JSON.stringify(state))
+        newState.inputValue = action.value
+        return newState
+    }
+    if (action.type == ADD_ITEM) {
+        let newState = JSON.parse(JSON.stringify(state)) //深拷贝
+        newState.list.push(newState.inputValue)
+        newState.inputValue = ''
+        return newState
+    }
+    if (action.type == DELETE_ITEM) {
+        let newState = JSON.parse(JSON.stringify(state)) //深拷贝原始仓库数据
+        newState.list.splice(action.index,1)
+        return newState
+    }
     return state
 }
